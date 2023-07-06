21219067564e63d8fe6b20114e935ea15f894459
rezellme <remo.zellmer@xailabs.de>
2023-07-06 18:46:06 +0200

    update hasura-auth v2.5

--- hasura-auth/build.gradle.kts
+++ hasura-auth/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 description = "Webhook implementation for Hasura authentication."
 group = "com.xailabs.microservices"
-version = "2.0.0"
+version = "2.5.55"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
